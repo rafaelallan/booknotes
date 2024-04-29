138fc21e831b4ce1237768c401c9cedc99fe7a8d
Rafael <rafael.allan@outlook.com>
2024-04-29 23:14:14 +0100

    Added bootstrap and made adjustments to elements

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
+// app.use(express.static(__dirname, "../public"));
+app.use('/notes', express.static('public'))
+app.use('/books', express.static('public'))
 
 app.get("/", (req, res) => {
     res.render("index.ejs");
