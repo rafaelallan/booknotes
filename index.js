11392b6b00e8615067b2b76fd5e1f10fdc323d90
Rafael <rafael.allan@outlook.com>
2024-05-01 19:59:39 +0100

    Change /book/new for /books route and newbooks.ejs to books.ejs for better clarity

--- index.js
+++ index.js
@@ -1,28 +1,36 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
-// app.use(express.static(__dirname, "../public"));
 app.use('/notes', express.static('public'))
 app.use('/books', express.static('public'))
+app.use(bodyParser.urlencoded(import.meta.url));
 
 app.get("/", (req, res) => {
     res.render("index.ejs");
 });
 
+// Notes
 app.get("/notes/new", (req, res) => {
     res.render("newnote.ejs");
-});
-
-app.get("/books/new", (req, res) => {
-    res.render("newbook.ejs");
 });
 
 app.get("/notes/id", (req, res) => {
     res.render("notes.ejs");
 });
+
+// ********** Books **********
+app.route("/books")
+    .get((req, res) => {
+        res.render("books.ejs");
+    })
+    .post((req, res) => {
+        console.log(req.body);
+        res.redirect("/books");
+    })
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
