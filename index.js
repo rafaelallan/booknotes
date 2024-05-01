6c7e9f75001e0ef490cc0417657f99a74d5f39b3
Rafael <rafael.allan@outlook.com>
2024-05-01 23:24:49 +0100

    Add insert book functionality

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
+const db = new pg.Client({
+    user: "postgres",
+    host: "localhost",
+    database: "booknotes",
+    password: "ImDBA",
+    port: 5432
+})
+
+db.connect();
 
 app.use(express.static("public"));
 app.use('/notes', express.static('public'))
 app.use('/books', express.static('public'))
-app.use(bodyParser.urlencoded(import.meta.url));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
     res.render("index.ejs");
@@ -22,15 +32,39 @@ app.get("/notes/id", (req, res) => {
     res.render("notes.ejs");
 });
 
+
+
 // ********** Books **********
+// app.get("/books", (req, res) => {
+//     res.render("books.ejs");
+// });
+
+// app.post("/books", (req, res) => {
+//     res.render("books.ejs");
+// });
+
+
 app.route("/books")
     .get((req, res) => {
         res.render("books.ejs");
     })
-    .post((req, res) => {
+    .post(async (req, res) => {
+        const bookTitle = req.body.bookTitle;
+        const actorName = req.body.actorName;
+        const isbn = Number(req.body.isbn);
+        const summary = req.body.summary;
+
         console.log(req.body);
         res.redirect("/books");
+
+        try {
+            await db.query("INSERT INTO books (title, summary, actor, isbn) VALUES ($1, $2, $3, $4)",[bookTitle, summary, actorName, isbn]);
+        } catch (err) {
+            console.error(err.stack);
+        };
     })
+
+// db.end();
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
